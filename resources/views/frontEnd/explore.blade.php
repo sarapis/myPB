8f73b965ec1463bcf4876a200897cd35de0bf790
loveisbluenova <danielkent910@gmail.com>
2018-02-19 17:52:32 -0300

    explore

--- resources/views/frontEnd/explore.blade.php
+++ resources/views/frontEnd/explore.blade.php
@@ -8,15 +8,7 @@ Explore
     @include('layouts.sidebar')
     <!-- Page Content Holder -->
     <div id="content">
-        <!-- Example Range -->
-        <div class="example-wrap">
-            <h4 class="example-title">Range</h4>
-            <div class="example mt-30">
-              <div class="asRange" data-plugin="asRange" data-namespace="rangeUi" data-step="10"
-              data-min="2" data-range="true" data-tip=true data-value="[10,70]"></div>
-            </div>
-        </div>
-        <!-- End Example Range -->
+        
     </div>
 </div>
 @endsection
