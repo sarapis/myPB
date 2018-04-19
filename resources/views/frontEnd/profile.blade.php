4a9f1818ff85a9c1d98096dee4124ccf774d20fc
Wouter Cleymans <woutercleymans.dev@gmail.com>
2018-04-19 14:46:00 -0300

    map

--- resources/views/frontEnd/profile.blade.php
+++ resources/views/frontEnd/profile.blade.php
@@ -2,7 +2,7 @@
 @section('title')
 Profile
 @stop
-<meta name="description" content="{{$project->profile_title}}">
+<meta name="description" content="{{$project->project_title}}">
 <style>
    .navbar-container.container-fluid{
         display: none !important;
