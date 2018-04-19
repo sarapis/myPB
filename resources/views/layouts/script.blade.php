13f5461e6356b5625e13981d44c4b0ec0ad2e237
Wouter Cleymans <woutercleymans.dev@gmail.com>
2018-04-19 11:17:44 -0300

    sidebar update

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -100,7 +100,7 @@
           });
           $('#sidebarCollapse').on('click', function () {
               $('#sidebar, #content').toggleClass('active');
-              $('.overlay').fadeIn();
+              $('.overlay').fadeToggle();
               $('.collapse.in').toggleClass('in');
               $('a[aria-expanded=true]').attr('aria-expanded', 'false');
           });
