9654d952d583b72f9acb0c96f145d4113b179fe5
Wouter Cleymans <woutercleymans.dev@gmail.com>
2018-05-09 15:52:09 -0300

    alert

--- resources/views/backEnd/pages/index.blade.php
+++ resources/views/backEnd/pages/index.blade.php
@@ -7,17 +7,17 @@ Page
 
     <h3>Pages <a href="{{ url('pages/create') }}" class="btn btn-primary pull-right btn-sm">Add New Page</a></h3>
     <div class="table table-responsive">
-        <table class="table table-bordered table-striped table-hover" id="tblpages">
+        <table class="table table-bordered table-striped table-hover">
             <thead>
                 <tr>
-                    <th>ID</th><th>Name</th><th>Title</th><th>Body</th><th>Actions</th>
+                    <th>ID</th><th>Name</th><th>Title</th><th>Actions</th>
                 </tr>
             </thead>
             <tbody>
             @foreach($pages as $item)
                 <tr>
                     <td>{{ $item->id }}</td>
-                    <td><a href="{{ url('pages', $item->id) }}">{{ $item->name }}</a></td><td>{{ $item->title }}</td><td>{{ $item->body }}</td>
+                    <td><a href="{{ url('pages', $item->id) }}">{{ $item->name }}</a></td><td>{{ $item->title }}</td>
                     <td>
                         <a href="{{ url('pages/' . $item->id . '/edit') }}" class="btn btn-primary btn-xs">Update</a> 
                         {!! Form::open([
